40e52d72b019e7556172625cc3d667f55fb5ed33
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-06-21 14:49:01 -0300

    Tasa 18% - Asistencia financiera

--- resources/views/base/frontendaf.blade.php
+++ resources/views/base/frontendaf.blade.php
@@ -48,33 +48,16 @@
 
     <div class="row mb-5">
         <div class="col-xs-12 col-sm-12 col-lg-12">
-            <h4 class="mt-3 mb-5">Objetivos</h4>
-
-            <p>
-                El Programa busca favorecer la sostenibilidad de las micro, pequeñas y medianas empresas (MiPyMEs)
-                entrerrianas que se hayan visto comprometidas en sus ingresos y/o cadenas de pago como consecuencia de
-                la pandemia internacional del COVID-19, y que cuenten por ello con dificultades para su sostenimiento
-                pasibles de ser acreditadas.
-            </p>
-
-        </div>
-    </div>
-
-
-    <div class="row mb-5">
-        <div class="col-xs-12 col-sm-12 col-lg-12">
             <h4 class="mt-3 mb-5">Requisitos</h4>
 
             <li class=" p-2">Ser persona física o jurídica constituida en la provincia de Entre Ríos.</li>
             <li class=" p-2">Contar con certificado MiPyME vigente.</li>
             <li class=" p-2">Estar inscriptos en la AFIP (Administración Federal de Ingresos Públicos) y en
-                ATER
-                (Administración Tributaria de Entre Ríos).</li>
+                ATER (Administración Tributaria de Entre Ríos).</li>
             <li class=" p-2">Tener un mínimo de un (1) año de antigüedad en la actividad objeto del proyecto.
             </li>
             <li class=" p-2">Cumplir con la normativa exigida para el desarrollo de su actividad, las normas
-                de higiene
-                y seguridad industrial exigibles y la normativa ambiental exigible.</li>
+                de higiene y seguridad industrial exigibles y la normativa ambiental exigible.</li>
 
         </div>
     </div>
@@ -84,24 +67,14 @@
             <h4 class="mt-3 mb-5">Financiamiento</h4>
 
             <p class=" p-2">Créditos de hasta <strong> $3.000.000</strong> (PESOS TRES MILLONES)
-                para
-                capital de
-                trabajo o inversión y capital de trabajo asociado. Se financiará hasta el CIEN POR CIENTO (100%) del
+                para capital de trabajo o inversión y capital de trabajo asociado. Se financiará hasta el CIEN POR CIENTO (100%) del
                 proyecto presentado. El monto solicitado no podrá superar, en ningún caso, el 50% del total de la
                 facturación anual del ejercicio anterior por parte de la empresa.</p>
 
 
             <li class=" p-2">Garantía: a sola firma (pagaré). A satisfacción de la evaluación del riesgo
-                crediticio por
-                parte de la
-                Secretaría a partir del proyecto presentado.</li>
-            <li class=" p-2">Tasa: 50% de la Tasa Activa de Cartera General Nominal Anual en pesos del Banco
-                de la
-                Nación Argentina,
-                menos 2 puntos porcentuales. Además, las empresas lideradas por mujeres podrán acceder a una reducción
-                de tasa de 2 puntos porcentuales mas. En ningún caso la tasa resultante podrá superar el 30%, que se
-                establece como tope máximo de interés a aplicar al financiamiento otorgado mediante el presente
-                Programa.</li>
+                crediticio por parte de la Secretaría a partir del proyecto presentado.</li>
+            <li class=" p-2">Tasa: <strong>18% de la TNA fijas</strong>. Además, las empresas lideradas por mujeres podrán acceder a una reducción de tasa de 2 puntos porcentuales más.</li>
             <li class=" p-2">Sistema de amortización: francés.</li>
             <li class=" p-2">Plazo de gracia: 6 meses, para capital de trabajo y 12 meses para inversión y
                 capital de
