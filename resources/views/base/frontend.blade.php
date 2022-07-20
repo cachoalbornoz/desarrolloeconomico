58b22018be7e87c917e5c3dc825e32b006a8f9eb
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-07-20 12:37:27 -0300

    Fix Home page  - Wtih Youtube videos

--- resources/views/base/frontend.blade.php
+++ resources/views/base/frontend.blade.php
@@ -27,21 +27,21 @@
 
         <div class="row mb-5 mt-5">
             <div class="col">
-                
-                <nav class="navbar navbar-expand-lg navbar-light bg-dark favenir pb-3">            
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-dark favenir pb-3">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
                         aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-            
+
                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            
+
                         <ul class="navbar-nav nav-fill w-100">
-                            
-            
+
+
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink"
+                                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     CREDITOS PRODUCTIVOS
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -53,24 +53,24 @@
                                     </a>
                                     <hr>
                                     <h6 class="ml-2">* Emprendedores</h6>
-            
+
                                     <a class="dropdown-item"
                                         href="https://www.entrerios.gov.ar/desarrolloemprendedor/frontend/programa_jovenes.php">
                                         - Créditos a tasa cero
                                     </a>
-            
+
                                     <a class="dropdown-item"
                                         href="https://www.entrerios.gov.ar/desarrolloemprendedor/frontend/programa_proaccer.php">
                                         - ANR para comercialización
                                     </a>
-            
+
                                     <a class="dropdown-item"
                                         href="https://www.entrerios.gov.ar/desarrolloemprendedor/frontend/programa_proceder.php">
                                         - ANR para innovación
                                     </a>
-            
+
                                     <hr />
-            
+
                                     <a class="dropdown-item" href="{{ route('pcapitalTrabajo') }}">
                                         - Banco de Entre Ríos
                                     </a>
@@ -80,29 +80,29 @@
                                     <a class="dropdown-item" href="{{ route('programarp') }}">
                                         - Reactivación Productiva
                                     </a>
-            
+
                                 </div>
                             </li>
-            
+
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink"
+                                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     CAPACITACIONES y ASISTENCIA TECNICA
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            
+
                                     <h6 class="ml-2 mr-2">Empresas y Cooperativas </h6>
-            
+
                                     <a class="dropdown-item" href="{{ route('pafinanciera') }}">
                                         - Empresas
                                     </a>
-            
+
                                     <a class="dropdown-item" href="{{ route('patecnica') }}">
                                         - Asistencia Técnica
                                     </a>
-            
+
                                     <hr />
-            
+
                                     <h6 class="ml-2">Emprendedores</h6>
                                     <a class="dropdown-item"
                                         href="https://www.entrerios.gov.ar/desarrolloemprendedor/frontend/programa_formacion.php">
@@ -112,18 +112,18 @@
                                         href="https://www.entrerios.gov.ar/desarrolloemprendedor/frontend/programa_tutorias.php">
                                         - Tutorías
                                     </a>
-            
+
                                 </div>
-                            </li>            
-            
+                            </li>
+
                         </ul>
-            
+
                     </div>
                 </nav>
 
             </div>
         </div>
-        
+
 
         <div class="row">
             <div class="col-xs-12 col-md-6 col-lg-6 w-100 mb-3">
@@ -140,9 +140,64 @@
                     </a>
                 </div>
             </div>
-        </div>       
+        </div>
 
-        
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-lg-12">
+                <h2 class=" mt-5 mb-5">Trayectorías de Jóvenes Emprendedores </h2>
+
+                <ul>
+                    {{--  --}}
+                    <h4 class="mb-3">Caravan Café y Crepes</h4>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe src="https://www.youtube.com/embed/Dt8ogUJgvzA" title="Caravan Café y Crepes"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
+
+                    {{--  --}}
+                    <h4 class="mt-5 mb-3">Cocomelón</h4>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe src="https://www.youtube.com/embed/H1Qjf5uD3yM" title="Cocomelón" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
+
+                    {{--  --}}
+                    <h4 class="mt-5 mb-3">Micro Digital Tecnología Forense</h4>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe src="https://www.youtube.com/embed/uUIdZDCtF-A" title="Micro Digital Tecnología Forense"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
+
+                    {{--  --}}
+                    <h4 class="mt-5 mb-3">Luthier</h4>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/sFd9hsyyAWM"
+                            title="Luthier" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
+
+                    {{--  --}}
+                    <h4 class="mt-5 mb-3">Eco Diseño Paraná</h4>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe src="https://www.youtube.com/embed/EY_GwrvaFtg" title="Eco Diseño Paraná" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+
+                    {{--  --}}
+                    <h4 class="mt-5 mb-3">Cooperativa de Trabajo Tenondé Limitada</h4>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/W3crBn2yzgk" title="Cooperativa de Trabajo Tenondé Limitada" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+
+                </ul>
+
+            </div>
+        </div>
 
 
 
