dec3bc77c201091cdfb694faa6662eacf93d32c8
Guillermo Albornoz <cachoalbornoz@gmail.com>
2023-01-18 12:28:34 -0300

    Arreglo link noticias

--- resources/views/base/noticias.blade.php
+++ resources/views/base/noticias.blade.php
@@ -1,80 +1,87 @@
-@if($noticias->count() >0)
+@if ($noticias->count() > 0)
 
 
-<div class="row mt-5 mb-5">
-    <h2 class="favenir">Noticias recientes </h2>
-</div>
+    <div class="row mt-5 mb-5">
+        <h2 class="favenir">
+            Noticias recientes
+        </h2>
 
-
-
-<div class="row mt-5 mb-5">
-    <div class="col-xs-12 col-sm-12 col-lg-12">
-
-        @foreach ($noticias->take(3) as $index => $noticia)
-
-            @if($index == 0)
-                @php
-                    $titulo0    = $noticia->titulo;
-                    $subtitulo0 = ($noticia->subtitulo)?$noticia->subtitulo:' ';
-                    $imagen0    = $noticia->imagen;
-                @endphp
-            @endif
-            
-            @if($index == 1)
-                @php
-                    $titulo1    = $noticia->titulo;
-                    $subtitulo1 = ($noticia->subtitulo)?$noticia->subtitulo:' ';
-                    $imagen1    = $noticia->imagen;
-                @endphp
-            @endif
-
-            @if($index == 2)
-                @php
-                    $titulo2    = $noticia->titulo;
-                    $subtitulo2 = ($noticia->subtitulo)?$noticia->subtitulo:' ';
-                    $imagen2    = $noticia->imagen;
-                @endphp
-            @endif
-
-        @endforeach
-
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="/economicoemprendedor/images/upload/noticias/{{ $imagen0 }}" alt="1 noticia">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>{{ $titulo0 }}</h5>
-                        <p>{{ $subtitulo0 }}</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="/economicoemprendedor/images/upload/noticias/{{ $imagen1 }}" alt="2 notica">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>{{ $titulo1 }}</h5>
-                        <p>{{ $subtitulo1 }}</p>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Anterior</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Siguiente</span>
-            </a>
-        </div>
-
+        <a class="ml-2 mt-2" href="{{ route('noticias.publicacion') }}">
+            (+ info)
+        </a>
 
     </div>
-</div>
 
 
-{{-- <div class="offset-2 col-8">
+
+    <div class="row mt-5 mb-5">
+        <div class="col-xs-12 col-sm-12 col-lg-12">
+
+            @foreach ($noticias->take(3) as $index => $noticia)
+                @if ($index == 0)
+                    @php
+                        $titulo0 = $noticia->titulo;
+                        $subtitulo0 = $noticia->subtitulo ? $noticia->subtitulo : ' ';
+                        $imagen0 = $noticia->imagen;
+                    @endphp
+                @endif
+
+                @if ($index == 1)
+                    @php
+                        $titulo1 = $noticia->titulo;
+                        $subtitulo1 = $noticia->subtitulo ? $noticia->subtitulo : ' ';
+                        $imagen1 = $noticia->imagen;
+                    @endphp
+                @endif
+
+                @if ($index == 2)
+                    @php
+                        $titulo2 = $noticia->titulo;
+                        $subtitulo2 = $noticia->subtitulo ? $noticia->subtitulo : ' ';
+                        $imagen2 = $noticia->imagen;
+                    @endphp
+                @endif
+            @endforeach
+
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100"
+                            src="/economicoemprendedor/images/upload/noticias/{{ $imagen0 }}" alt="1 noticia">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>{{ $titulo0 }}</h5>
+                            <p>{{ $subtitulo0 }}</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100"
+                            src="/economicoemprendedor/images/upload/noticias/{{ $imagen1 }}" alt="2 notica">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>{{ $titulo1 }}</h5>
+                            <p>{{ $subtitulo1 }}</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Siguiente</span>
+                </a>
+            </div>
+
+
+        </div>
+    </div>
+
+
+    {{-- <div class="offset-2 col-8">
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             @foreach ($noticias as $noticia)
@@ -93,10 +100,10 @@
     </div>
 </div> --}}
 
-<div class="row mt-5">
-    <div class="col-12">
-        &nbsp;
+    <div class="row mt-5">
+        <div class="col-12">
+            &nbsp;
+        </div>
     </div>
-</div>
 
 @endif
