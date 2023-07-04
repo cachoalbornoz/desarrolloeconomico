203f5733574c21a6b0ce7309f528d1a869e64a92
Guillermo Albornoz <cachoalbornoz@gmail.com>
2023-07-04 08:26:22 -0300

    Imagenes portada Empresas - Jovenes

--- resources/views/base/frontend.blade.php
+++ resources/views/base/frontend.blade.php
@@ -122,14 +122,15 @@
             <div class="col-xs-12 col-md-6 col-lg-6 w-100 mb-3">
                 <div class="mb-3 text-center h-100">
                     <a href="{{ route('programasp') }}">
-                        <img src="{{ asset('public/images/frontend/portadapymes.png') }}" class=" img-fluid img-medium" />
+                        <img src="{{ asset('public/images/frontend/portadapymes_new.png') }}"
+                            class=" img-fluid img-medium" />
                     </a>
                 </div>
             </div>
             <div class="col-xs-12 col-md-6 col-lg-6 w-100 mb-3">
                 <div class="mb-3 text-center h-100">
                     <a href="https://www.entrerios.gov.ar/desarrolloemprendedor" target="_blank">
-                        <img src="{{ asset('public/images/frontend/portadad.jpeg') }}" class=" img-fluid img-medium" />
+                        <img src="{{ asset('public/images/frontend/portadad_new.png') }}" class=" img-fluid img-medium" />
                     </a>
                 </div>
             </div>
@@ -137,7 +138,7 @@
 
         <div class="row favenir">
             <div class="col-xs-12 col-sm-12 col-lg-12">
-                <h2 class=" mt-5 mb-5">Trayectorías de Jóvenes Emprendedores </h2>
+                <h3 class=" mt-5 mb-5">Trayectorías de Jóvenes Emprendedores </h3>
 
                 <div class="row mt-3">
                     <div class="col-6">
