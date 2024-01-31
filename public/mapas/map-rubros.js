c6350e1c2c1484e6e04cb995f74710c2a9fcb04f
Guillermo Albornoz <cachoalbornoz@gmail.com>
2024-01-31 12:36:23 -0300

    Agregado de departamentos - Sidebar

--- public/mapas/map-rubros.js
+++ public/mapas/map-rubros.js
@@ -9,13 +9,16 @@ let map = L.map('map', {
         { position: 'topright' }
 }).setView([-31.95528, -57.97186], 8);
 
+// Referencias
+const sidebar = document.querySelector('#sidebar');
+const alert = document.querySelector('#alert');
+
 
 // Hacer zoom sobre los departamentos
-document.getElementById('departamentos').addEventListener('change', function (e) {
-    let coords = e.target.value.split(",");
-    map.flyTo(coords, 14);
-})
-
+// document.getElementById('departamentos').addEventListener('change', function (e) {
+//     let coords = e.target.value.split(",");
+//     map.flyTo(coords, 14);
+// })
 
 let bingkey = 'Av1z4G0ITtfkMdxUW0qsvJHvYZbjDXOVibWwMhCmEJxAXf-YHYL1uoRjU9YBE-s6';
 let imagerySet = "AerialWithLabels"; // AerialWithLabels | Birdseye | BirdseyeWithLabels | Road
@@ -25,7 +28,6 @@ let streetView = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 let roadmap = L.tileLayer.bing(bingkey, { type: imagerySet });
-
 
 var baseLayers = {
     "Politico": politico,
@@ -39,10 +41,28 @@ politico.addTo(map);
 new L.control.mousePosition({ position: 'topright' }).addTo(map);
 new L.control.scale({ imperial: false }).addTo(map);
 
+// Cuadro de Informacion
+// Agregar control para ver los datos al pasar el puntero
+
+var info = L.control();
+
+// Crear un div con una clase info
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+// Agregar el metodo que actualiza el control segun el puntero vaya pasando
+info.update = function (props) {
+    this._div.innerHTML = '<h6>Mapa - Rubros productivos</h6>';
+};
+info.addTo(map);
+
+
+
 // Dibujar marcadores de las empresas
 empresas.forEach(empresa => {
-
-    
     L.circleMarker(L.latLng(empresa.longitud, empresa.latitud), {
         radius: 6,
         fillColor: "#ff0000",
@@ -53,16 +73,55 @@ empresas.forEach(empresa => {
     }).addTo(map);
 })
 
+//Crear listado de lugares
+
+const volar = (coord) => {
+    const zoom = map.getMaxZoom();
+    map.flyTo(coord,14)
+}
+
+const definirAlert = ([latitud,longitud]) => {
+    alert.classList.remove('hidden');
+    alert.innerText = `Coordenadas:
+        Latitud: ${latitud},
+        Longitud: ${longitud}
+        `
+}
 
 
+// Funcion para listar los sites
 
+// Primero limpiar el Active de cada item
+const limpiarItems = () => {
+    const listadoLi = document.querySelectorAll('li');
+    listadoLi.forEach(li => {
+        li.classList.remove('active');
+    })
+}
 
+// Crear el listado
+const crearListado = () => {
+    const ul = document.createElement('ul');
+    ul.classList.add('list-group');
+    sidebar.prepend(ul);
 
+    departamentos.forEach(lugar => {
+        const li = document.createElement('li');
+        li.innerText = lugar.nombre;
+        li.classList.add('list-group-item');
+        ul.append(li);
 
+        li.addEventListener('click', () => {
+            limpiarItems();
+            li.classList.add('active');
+            volar(lugar.coordenadas);
+            definirAlert(lugar.coordenadas);
+        })
+    })
 
+}
 
-
-
+crearListado();
 
 
 
--- resources/views/admin/mapas/rubros.blade.php
+++ resources/views/admin/mapas/rubros.blade.php
@@ -6,36 +6,20 @@
 
 @section('content')
 
-    <div>
+    <div class="d-flex h-100">
 
-        Mapa de empresas x rubros 
+        <div class="row m-0 w-100">
 
-        <select name="departamentos" id="departamentos">
-            <option value="">Seleccione un departamento</option>
-            <option value="-32.2245289,-58.1675946">Colon</option>
-            <option value="-31.3785815,-58.082821">Concordia</option>
-            <option value="-32.06526,-60.60565">Diamante</option>
-            <option value="-30.9927822,-57.9447917">Federacion</option>
-            <option value="-30.9499301,-58.7950395">Federal</option>
-            <option value="-30.4039124,-58.7921657">Feliciano</option>
-            <option value="-33.1446543,-59.3354523">Gualeguay</option>
-            <option value="-33.00802,-58.51765">Gualeguaychu</option>
-            <option value="-33.5963439,-59.3304068">Islas del Ibicuy</option>
-            <option value="-30.7458081,-59.6389937">La Paz</option>
-            <option value="-32.3974349,-59.8228071">Nogoya</option>
-            <option value="-31.74087,-60.52308">Parana</option>
-            <option value="-31.6237211,-58.5463309">San Salvador</option>
-            <option value="-32.3066109,-59.1579668">Tala</option>
-            <option value="-32.4737569,-58.3112324">Uruguay</option>
-            <option value="-32.61596,-60.15092">Victoria</option>
-            <option value="-31.86168,-59.02911">Villaguay</option>
-        </select>
-    </div>
+            <div id="sidebar" class="d-flex flex-column justify-content-between col-2 h-100">
+                <div id="alert" class="w-100 alert alert-warning fw-bold hidden" role="alert"></div>
+            </div>
 
-    <div id="map">
+            <div id="map" class="col"> </div>
+        </div>
 
     </div>
 
+    <script src="{{ asset('/public/mapas/departamentos.js') }}"></script>
     <script src="{{ asset('/public/mapas/empresas.js') }}"></script>
     <script src="{{ asset('/public/mapas/map-rubros.js') }}"></script>
 
